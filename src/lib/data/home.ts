5b9ee8dc566924ccdda6491d565724fb47d22869
svyepofficial <svyep.official@gmail.com>
2026-04-15 11:41:36 -0700

    Update home.ts

--- src/lib/data/home.ts
+++ src/lib/data/home.ts
@@ -1,8 +1,8 @@
 const home = {
 	statistics: {
-		members: 1700,
-		raised: 295000,
-		impacted_students: 210000,
+		members: 1900,
+		raised: 305000,
+		impacted_students: 215000,
 		internships: 25,
 		businesses_worked_with: 60
 	}
